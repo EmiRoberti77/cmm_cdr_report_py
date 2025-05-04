d5f61c214085671c7b5208fba890614403dd4195
EmiRoberti77 <emiliano.roberti@accenture.com>
2025-05-04 18:33:49 +0100

    confidential processing

--- cmm_report_generator.py
+++ cmm_report_generator.py
@@ -1,8 +1,29 @@
 import json
 import random
+from google.cloud import storage
+from list_buckets import get_project_name
+
+BUCKET_NAME = 'cmm_reports'
 
 def generate_report(provider):
-  return {
+  report = {
     "provider":provider,
-    "reach":random.random(5_000_000, 15_000_000)
+    "reach":random.randint(5_000_000, 15_000_000)
   }
+  print(json.dumps(report, indent=2))
+  return report
+
+
+def upload_to_gcs(bucket_name,filename, data):
+  client = storage.Client(project=get_project_name())
+  bucket = client.bucket(bucket_name)
+  blob = bucket.blob(filename)
+  blob.upload_from_string(json.dumps(data), content_type="application/json")
+  print(f"file uploaded {filename} to {bucket_name}")
+
+if __name__ == "__main__":
+  providers = ["TV", "Meta", "Google"]
+  for provider in providers:
+    data = generate_report(provider)
+    filename = f"{provider}_report.json"
+    upload_to_gcs(BUCKET_NAME, filename, data)    
---
+++ confidential_vm_processing.py
@@ -0,0 +1,29 @@
+import json
+from google.cloud import storage
+from list_buckets import get_project_name
+
+IN_BUCKET_NAME = 'cmm_reports'
+OUT_BUCKET_NAME = 'cmm_reports_out'
+
+def download_reports(bucket_name):
+  client = storage.Client(project=get_project_name())
+  bucket = client.bucket(bucket_name)
+  blobs = list(bucket.list_blobs())
+  reports = []
+  for blob in blobs:
+    content = blob.download_as_string()
+    report = json.loads(content)
+    reports.append(report)
+
+  return reports
+
+def compute_cmm_metrics(reports):
+  gross_reach = sum(r["reach"] for r in reports)
+  duplication = int(gross_reach * 0.15)
+  net_reach = gross_reach - duplication
+  return {
+        "gross_reach": gross_reach,
+        "duplication": duplication,
+        "net_reach": net_reach
+    }
+
